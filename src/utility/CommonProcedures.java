b7d71d43974f69c7840a50e84dda172f025ec9bc
Gaurav Vyas <vyasg@pbworld.com>
2018-12-18 14:19:05 -0500

    Bug fix in calculating minutes to interval for MAG

--- src/utility/CommonProcedures.java
+++ src/utility/CommonProcedures.java
@@ -7,27 +7,40 @@ public class CommonProcedures {
 	
 	//Convert interval into minutes
 	public static float convertIntervalToMinutes( int interval ) {
-		float minutes = ( interval - 0.5f ) * 15;
+		float minutes = ( interval - 0.5f ) * Constants.TOD_INTERVAL_IN_MINUTES;
 		return minutes;
 	}
 	
 	public static float convertIntervalToMinutesForDepartFinalTripSequence( int interval ) {
-		float minutes = ( interval-1) * 15 +.01f;
+		float minutes = ( interval-1) * Constants.TOD_INTERVAL_IN_MINUTES +.01f;
 		return minutes;
 	}
 	
 	public static float convertIntervalToMinutesForArriveFinalTripSequence( int interval ) {
-		float minutes = ( interval ) * 15 - .01f;
+		float minutes = ( interval ) * Constants.TOD_INTERVAL_IN_MINUTES - .01f;
 		return minutes;
 	}
 	
 	
-	
-	
+	// MAG Specific
+	public static int getInterval( int hourFrom3Am, int minute ) {
+		
+		int hour = hourFrom3Am + 3;
+		int hr = 0;
+		if ( hour > 24 )
+			hr = Constants.NUM_TOD_INTERVALS;		
+		else if ( hour < 4 )
+			hr = 1;		
+		else
+			hr = hour - 4;		
+		int interval = hr*Constants.NUM_TOD_INTERVALS_PER_HOUR + ( minute < Constants.TOD_INTERVAL_IN_MINUTES ? 0 : 1 );
+		
+		return Math.max( 1, Math.min( Constants.NUM_TOD_INTERVALS, interval ) );
+	}
 	
 	//Convert minutes into intervals
 	public static int convertMinutesToInterval( float minutes ) {
-		int interval = (int)( ( minutes / 15 ) + 0.99999 );
+		int interval = (int)( ( minutes / Constants.TOD_INTERVAL_IN_MINUTES ) + 0.99999 );
 		return Math.min( 96, interval );
 	}
 	
