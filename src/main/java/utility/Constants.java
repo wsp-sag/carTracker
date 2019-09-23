ab482118af887e36e3f08e6108cd9822724afd32
Sijia Wang <wangsijia0628@gmail.com>
2019-09-23 12:18:47 -0400

    1.  change carTracker condition to run for all households 2.  do not write out any ABM taxi trips, only those taxi trips converted from auto 3.  Ohio time interval is 15mins

--- src/main/java/utility/Constants.java
+++ src/main/java/utility/Constants.java
@@ -3,9 +3,9 @@ package utility;
 // This class contains the constants which are used by several models
 public class Constants {
 	
-    public static final int TOD_INTERVAL_IN_MINUTES = 30;    
-    public static final int NUM_TOD_INTERVALS_PER_HOUR = 2;    
-    public static final int NUM_TOD_INTERVALS = 40; 
+    public static final int TOD_INTERVAL_IN_MINUTES = 15;    
+    public static final int NUM_TOD_INTERVALS_PER_HOUR = 4;    
+    public static final int NUM_TOD_INTERVALS = 96; 
       
     public static final int DEFALUT_LAST_DEPART_MINUTE = 1430;
     
